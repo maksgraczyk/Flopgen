5a1e9fd3a858dc4ee7aacdee2b7acab4a468f01d
Maksymilian Graczyk <maksymilian.graczyk18@imperial.ac.uk>
2020-10-01 23:24:16 +0200

    Create main.cpp with CLI parsing

---
+++ main.cpp
@@ -0,0 +1,219 @@
+#include "classes.hpp"
+#include <iostream>
+#include "cli/CLI11.hpp"
+#include <algorithm>
+
+using namespace std;
+
+struct Options {
+  string output_filename;
+  vector<string> filenames;
+  FloppySize floppy_size;
+  int code_page;
+};
+
+inline bool compare_file_sizes(File *f1, File *f2) {
+  return f1->get_size() > f2->get_size();
+}
+
+bool get_filenames(vector<string> &result, istream *input);
+bool run(Options &options);
+
+int main(int argc, char** argv) {
+  CLI::App app
+    {"Flopgen: a tool for automatic creation of IMG floppy images"};
+
+  Options options;
+
+  auto output_opt = app.add_option("-o,--output", options.output_filename,
+                                   "Set base output filename without .img. All images will be of form (name)1.img, (name)2.img etc.");
+  output_opt->required();
+
+  options.code_page = 437;
+  auto code_page_opt = app.add_option("-c,--codepage", options.code_page,
+                                      "Set code page (default: 437)");
+
+  int size = 1440;
+  auto size_opt = app.add_option("-s,--size", size,
+                                 "Set size of each image in kilobytes (default: 1440)");
+  size_opt->check(CLI::IsMember({360, 720, 1200, 1440, 2880}));
+
+  string input;
+  auto input_opt = app.add_option("-i,--input", input,\
+                                  "Set filename of input list of files separated by a new line (use - for stdin)");
+
+  vector<string> filenames;
+  auto filenames_opt = app.add_option("FILES/DIRS", filenames,
+                                      "Specify files/directories to be added to images (directories are processed recursively)");
+
+  CLI11_PARSE(app, argc, argv);
+
+  switch (options.code_page) {
+  case 437:
+  case 720:
+  case 737:
+  case 771:
+  case 775:
+  case 850:
+  case 852:
+  case 855:
+  case 857:
+  case 860:
+  case 861:
+  case 862:
+  case 863:
+  case 864:
+  case 865:
+  case 866:
+  case 869:
+  case 932:
+  case 936:
+  case 949:
+  case 950:
+    break;
+
+  default:
+    cerr << "The code page must be one of the following:" << endl;
+    cerr << "437, 720, 737, 771, 775, 850, 852, 855, 857, 860, "
+      "861, 862, 863, 864, 865, 866, 869, 932, 936, 949, 950" << endl;
+    return 1;
+  }
+
+  if (input == "" && filenames.size() == 0) {
+    cerr << "You must either specify files/directories to be added "
+      "to images or use -i/--input." << endl;
+    cerr << "Run with --help for more information." << endl;
+    return 106;
+  }
+
+  if (filenames.size() > 0) {
+    options.filenames = filenames;
+  } else if (input == "-") {
+    get_filenames(options.filenames, &cin);
+  } else {
+    ifstream stream(input);
+    bool result = get_filenames(options.filenames, &stream);
+    stream.close();
+
+    if (!result) {
+      return 1;
+    }
+  }
+
+  options.floppy_size = static_cast<FloppySize>(size);
+  
+  if (!run(options)) {
+    return 1;
+  }
+  
+  return 0;
+}
+
+bool get_filenames(vector<string> &result, istream *input) {
+  while (!input->eof()) {
+    string filename;
+    getline(*input, filename);
+
+    if (input->rdstate() != 0 && !input->eof()) {
+      cerr << "Could not read the list of files due to an error, aborting."
+           << endl;
+      return false;
+    }
+    
+    result.push_back(filename);
+  }
+
+  return true;
+}
+
+bool run(Options &options) {
+  vector<File *> files;
+
+  for (string &filename : options.filenames) {
+    File *file = File::get_instance(filename);
+    if (file != NULL) {
+      files.push_back(file);
+    }
+  }
+
+  if (files.size() == 0) {
+    cerr << "There are no files/directories to be added to images!" << endl;
+    return false;
+  }
+
+  sort(files.begin(), files.end(), compare_file_sizes);
+
+  Floppy *current_floppy = new Floppy(options.floppy_size, options.code_page);
+
+  if (current_floppy == NULL) {
+    cerr << "Could not allocate memory for a floppy image, aborting." << endl;
+    
+    for (File *file : files) {
+      delete file;
+    }
+
+    return false;
+  }
+
+  int count = 1;
+  
+  for (File *file : files) {
+    if (file->get_size() > current_floppy->get_size()) {
+      cerr << file->get_path_str() << " is larger than the image size, "
+        "ignoring." << endl;
+      continue;
+    }
+
+    if (!current_floppy->add_file(file)) {
+      if (!current_floppy->save(options.output_filename + to_string(count++)
+                                + ".img")) {
+        cerr << "Could not produce a floppy image number " << count - 1
+             << " due to an error, aborting." << endl;
+        
+        delete current_floppy;
+
+        for (File *file : files) {
+          delete file;
+        }
+
+        return false;
+      }
+
+      delete current_floppy;
+      current_floppy = new Floppy(options.floppy_size, options.code_page);
+
+      if (current_floppy == NULL) {
+        cerr << "Could not allocate memory for a floppy image, aborting."
+             << endl;
+        
+        for (File *file : files) {
+          delete file;
+        }
+
+        return false;
+      }
+
+      current_floppy->add_file(file);
+    }
+  }
+
+  bool result = current_floppy->save(options.output_filename + to_string(count)
+                                     + ".img");
+
+  delete current_floppy;
+
+  for (File *file : files) {
+    delete file;
+  }
+
+  if (!result) {
+    cerr << "Could not produce a floppy image number " << count
+         << " due to an error, aborting." << endl;
+    return false;
+  }
+
+  cout << "Successfully created " << count << " floppy image"
+       << (count > 1 ? "s" : "") << "." << endl;
+
+  return result;
+}
